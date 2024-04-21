80aaf78de63a679ce73721c3b44e187848cc30cf
prorange2023 <154119516+prorange2023@users.noreply.github.com>
2024-04-22 00:01:52 +0900

    feat : 복구 도중

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,6 +4,7 @@ public class GameManager : Singleton<GameManager>
 {
     [Header("Save")]
     [SerializeField] public int savePoint;
+    
     public void Test()
     {
         Debug.Log(GetInstanceID());
