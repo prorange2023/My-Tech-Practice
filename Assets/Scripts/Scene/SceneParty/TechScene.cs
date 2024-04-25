a73eb25d7ed6d1332c4d433a7a5078d026ee8c16
prorange2023 <154119516+prorange2023@users.noreply.github.com>
2024-04-25 17:55:46 +0900

    feat : Code Refactoring

--- Assets/Scripts/Scene/SceneParty/TechScene.cs
+++ Assets/Scripts/Scene/SceneParty/TechScene.cs
@@ -15,39 +15,34 @@ public class TechScene : MonoBehaviour
 
     public void LoadRespawn()
     {
-        switch (Manager.Game.savePoint)
+        int savePoint = Manager.Game.savePoint;
+        if (savePoint > 0 && savePoint <= EnemyGroups.Count)
         {
-            case 0:
-                break;
-            case 1:
-                //Monster[0].SetActive(false);
-                //Monster[1].SetActive(false);
-                //Monster[2].SetActive(false);
-                foreach (Enemy enemy in EnemyGroup1)
-                {
-                    enemy.gameObject.SetActive(false);
-                }
-                break;
-            case 2:
-                
-                for (int i = 0; i < 2;  i++)
-                {
-                    foreach (Enemy enemy in EnemyGroup1)
-                    {
-                        enemy.gameObject.SetActive(false);
-                    }
-                }
-
-
-                foreach (Enemy enemy in EnemyGroup1)
-                {
-                    enemy.gameObject.SetActive(false);
-                }
-                foreach (Enemy enemy in EnemyGroup2)
-                {
-                    enemy.gameObject.SetActive(false);
-                }
-                break;
+            foreach (Enemy enemy in EnemyGroups[savePoint - 1])
+            {
+                enemy.gameObject.SetActive(false);
+            }
         }
+        //switch (Manager.Game.savePoint)
+        //{
+        //    case 0:
+        //        break;
+        //    case 1:
+        //        foreach (Enemy enemy in EnemyGroup1)
+        //        {
+        //            enemy.gameObject.SetActive(false);
+        //        }
+        //        break;
+        //    case 2:
+        //        foreach (Enemy enemy in EnemyGroup1)
+        //        {
+        //            enemy.gameObject.SetActive(false);
+        //        }
+        //        foreach (Enemy enemy in EnemyGroup2)
+        //        {
+        //            enemy.gameObject.SetActive(false);
+        //        }
+        //        break;
+        //}
     }
 }
