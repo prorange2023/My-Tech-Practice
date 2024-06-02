0c779ad4466c915aa36aeef0ee84746eaebffd8b
prorange2023 <154119516+prorange2023@users.noreply.github.com>
2024-06-02 23:21:45 +0900

    feat : Visual Novel story mode

---
+++ Assets/Scripts/VisualNovel/GameController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public StoryScene currentScene;
+    public BottomBarController bottomBar;
+    public BackgroundController backgroundController;
+
+    private void Start()
+    {
+        bottomBar.PlayScene(currentScene);
+        backgroundController.SetImage(currentScene.background);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (bottomBar.IsCompleted())
+            {
+                if (bottomBar.IsLastSentence())
+                {
+                    currentScene = currentScene.nextScene;
+                    bottomBar.PlayScene(currentScene);
+                    backgroundController.SwitchImage(currentScene.background);
+                }
+                else
+                {
+                    bottomBar.PlayNextSentence();
+                }
+            }
+        }
+    }
+}
