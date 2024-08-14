6773a73c98ace8999ad89380ca6d8944a9c6005d
prorange2023@gmail.com <prorange2023@gmail.com>
2024-08-14 13:18:57 +0900

    Fix : 참조용 스크립트

--- Assets/Scripts/Puzzle/Penakoni Building/DreamButton.cs
+++ Assets/Scripts/Puzzle/Penakoni Building/DreamButton.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PuzzleButton : MonoBehaviour, IInterActable
 {
     public List<PenakoniBuildingPuzzle> buildingPuzzles;
-    public void Dreamer()
+
+    public void Interact(PlayerInterActor player)
     {
-        Debug.Log("dreamer");
+        StartCoroutine(Elebator(player));
+    }
+    
+    IEnumerator Elebator(PlayerInterActor player)
+    {
+        player.GetComponentInParent<PlayerSet>().GetComponent<Transform>().parent = transform;
         foreach (var buildingPuzzle in buildingPuzzles)
         {
             if (buildingPuzzle.puzzleOn == true)
@@ -19,9 +27,7 @@ public class PuzzleButton : MonoBehaviour, IInterActable
                 buildingPuzzle.puzzleOn = true;
             }
         }
-    }
-    public void Interact(PlayerInterActor player)
-    {
-        Dreamer();
+        yield return new WaitForSeconds(2f);
+        player.GetComponentInParent<PlayerSet>().GetComponent<Transform>().parent = null;
     }
 }
